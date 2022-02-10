887d2500cfde6915c8762fcf4857ba0c182cd4cb
Tomás Gonçalves <tomaslopesgoncalves@outlook.pt>
2022-02-10 17:01:55 +0000

    Switching player each turn

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		while (true) {
 			try {
 				UI.clerarScreen();
-				UI.printBoard(chessMatch.getPiece());
+				UI.printMathc(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -3,6 +3,7 @@ package aplication;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -28,23 +29,22 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	public static void clerarScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
-	
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		}
-		catch(RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. valid values are from a1 at h8");
 		}
-		
+
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -57,7 +57,14 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
+	public static void printMathc(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPiece());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Waitig player: " + chessMatch.getCurrentPlayer());
+	}
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -70,7 +77,7 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
-		if(background) {
+		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 		if (piece == null) {
